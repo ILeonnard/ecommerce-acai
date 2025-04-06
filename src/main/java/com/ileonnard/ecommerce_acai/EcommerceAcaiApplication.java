80c30c587dd7fbf0f7cd6136c1094d74a69d934c
ILeonnard <igorleonnard@gmail.com>
2025-04-06 11:28:30 -0300

    Project Created

---
+++ src/main/java/com/ileonnard/ecommerce_acai/EcommerceAcaiApplication.java
@@ -0,0 +1,13 @@
+package com.ileonnard.ecommerce_acai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceAcaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceAcaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ileonnard/ecommerce_acai/EcommerceAcaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ileonnard.ecommerce_acai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceAcaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
